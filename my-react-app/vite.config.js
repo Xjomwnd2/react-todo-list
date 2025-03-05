0c45e99aa517e7098de125d48dc8eba3643e2a84
Joel Ndiba Mwaura <mwa21001@byui.edu>
2025-03-05 05:16:52 +0300

    commit

--- my-react-app/vite.config.js
+++ my-react-app/vite.config.js
@@ -1,6 +1,10 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-});
+  root: 'my-react-app', // Set the root directory
+  server: {
+    port: 5173,
+  },
+})
